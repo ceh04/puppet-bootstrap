aa50dc1fcb86a9d13244469f191c7a465136205b
Charlie Hanner <channer@mitre.org>
2014-11-12 23:45:55 -0500

    Bumped installation versions

--- mac_os_x.sh
+++ mac_os_x.sh
@@ -11,8 +11,9 @@ set -e
 #--------------------------------------------------------------------
 # Modifiable variables, please set them via environmental variables.
 #--------------------------------------------------------------------
-FACTER_PACKAGE_URL=${FACTER_PACKAGE_URL:-"http://downloads.puppetlabs.com/mac/facter-1.7.2.dmg"}
-PUPPET_PACKAGE_URL=${PUPPET_PACKAGE_URL:-"http://puppetlabs.com/downloads/mac/puppet-3.2.3.dmg"}
+FACTER_PACKAGE_URL=${FACTER_PACKAGE_URL:-"http://downloads.puppetlabs.com/mac/facter-2.3.0.dmg"}
+HIERA_PACKAGE_URL=${HIERA_PACKAGE_URL:-"http://downloads.puppetlabs.com/mac/hiera-1.3.4.dmg"}
+PUPPET_PACKAGE_URL=${PUPPET_PACKAGE_URL:-"http://puppetlabs.com/downloads/mac/puppet-latest.dmg"}
 
 #--------------------------------------------------------------------
 # NO TUNABLES BELOW THIS POINT.
@@ -52,8 +53,9 @@ function install_dmg() {
 }
 
 # Install Puppet and Facter
-install_dmg "Puppet" "${PUPPET_PACKAGE_URL}"
 install_dmg "Facter" "${FACTER_PACKAGE_URL}"
+install_dmg "Hiera" "${HIERA_PACKAGE_URL}"
+install_dmg "Puppet" "${PUPPET_PACKAGE_URL}"
 
 # Hide all users from the loginwindow with uid below 500, which will include the puppet user
 defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool YES
